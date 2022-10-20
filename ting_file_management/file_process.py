5ec3c27e4946498378523db52e95c6f31e84de5e
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-10-20 17:19:26 -0300

    requisito 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+# from queue import Queue
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    for inst in instance._data:
+        if inst["nome_do_arquivo"] == path_file:
+            return "Arquivo já existe"
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data
+    }
+    instance.enqueue(result)
+    print(result, file=sys.stdout)
 
 
 def remove(instance):
@@ -8,3 +23,9 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+# queue = Queue()
+# process("statics/arquivo_teste.txt", queue)
+# print(queue._data)
+# print(process("statics/arquivo_teste.txt", queue))
