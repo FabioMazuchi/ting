93e050c3894d01e650883dfba7c0252885784367
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-10-20 20:37:53 -0300

    requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -6,6 +6,17 @@ def ocorrencias(word, lista):
     return result
 
 
+def ocorrencias_conteudo(word, lista):
+    result = []
+    for index in range(len(lista)):
+        if word.upper() in lista[index].upper():
+            result.append({
+                "linha": index + 1,
+                "conteudo": lista[index]
+            })
+    return result
+
+
 def verify_ocorrencias(lista):
     for data in lista:
         if len(data["ocorrencias"]) != 0:
@@ -30,4 +41,17 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for data in instance._data:
+        result.append(
+            {
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias":
+                    ocorrencias_conteudo(word, data["linhas_do_arquivo"])
+            }
+        )
+    if verify_ocorrencias(result):
+        return result
+    else:
+        return []
