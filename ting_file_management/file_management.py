e78b44bda312cf402609fda6ec55ad1f23c7444e
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-10-20 16:34:28 -0300

    requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,3 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
-# Inicio
+    if ".txt" not in path_file:
+        print("Formato inválido", file=sys.stderr)
+        return
+    try:
+        with open(path_file, "r") as file:
+            return file.read().split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
